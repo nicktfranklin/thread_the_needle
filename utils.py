184837dfb65ebdb8cf10231993347226c7c371ad
Nick Franklin <nthompsonfranklin@gmail.com>
2021-12-06 14:30:22 -0500

    initial commit of helper functions

---
+++ utils.py
@@ -0,0 +1,95 @@
+import numpy as np
+import scipy
+
+
+def define_valid_lattice_transitions(n_rows: int, n_columns: int) -> np.ndarray:
+    """
+    Defines a random transition matrix over a square lattice grid where transitions
+    are valid only between neighboring states
+    """
+
+    n_states = n_rows * n_columns
+
+    up_transitions = np.concatenate(
+        [np.zeros(n_columns), np.ones(n_columns * (n_rows - 1))]
+    ).reshape(-1, 1)
+
+    left_transitions = np.concatenate(
+        [np.zeros(1), np.ones(n_columns - 1)] * n_rows
+    ).reshape(-1, 1)
+
+    self_transitions = np.ones((n_states, 1))
+
+    right_transitions = np.concatenate(
+        [np.ones(n_columns - 1), np.zeros(1)] * n_rows
+    ).reshape(-1, 1)
+
+    down_transitions = np.concatenate(
+        [np.ones(n_columns * (n_rows - 1)), np.zeros(n_columns)]
+    ).reshape(-1, 1)
+    return np.concatenate(
+        [
+            up_transitions,
+            left_transitions,
+            self_transitions,
+            right_transitions,
+            down_transitions,
+        ],
+        axis=1,
+    )
+
+
+def convert_movements_to_transition_matrix(movements: np.ndarray) -> np.ndarray:
+    transition_matrix = np.diag(movements[:, 2], k=0)
+    transition_matrix += np.diag(movements[:-1, 3], k=1)
+    transition_matrix += np.diag(movements[1:, 1], k=-1)
+
+    n_cols = np.sum(movements[:, 0] == 0.0)
+    transition_matrix += np.diag(movements[n_cols:, 0], k=-n_cols)
+    transition_matrix += np.diag(movements[:-n_cols, 4], k=n_cols)
+    return transition_matrix
+
+
+def draw_dirichlet(valid_movements: np.ndarray) -> np.ndarray:
+    _p = np.random.dirichlet(np.ones(np.sum(valid_movements)))
+    p = np.zeros_like(valid_movements, dtype=float)
+    t = 0
+    for ii, isValid in enumerate(valid_movements):
+        if isValid:
+            p[ii] = _p[t]
+            t += 1
+    return p
+
+
+def draw_dirichlet_transitions(movements: np.ndarray) -> np.ndarray:
+    return np.array(list(map(draw_dirichlet, np.array(movements, dtype=bool))))
+
+
+def make_diffusion_transition(movements: np.ndarray) -> np.ndarray:
+    def _normalize(x):
+        return x / np.sum(x)
+
+    return np.array(list(map(_normalize, movements)))
+
+
+def sample_random_transition_matrix(n_rows: int, n_columns: int) -> np.ndarray:
+    movements = define_valid_lattice_transitions(n_rows, n_columns)
+    movement_probs = draw_dirichlet_transitions(movements)
+    return convert_movements_to_transition_matrix(movement_probs)
+
+
+def make_diffision_transition_matrix(n_rows: int, n_columns: int) -> np.ndarray:
+    movements = define_valid_lattice_transitions(n_rows, n_columns)
+
+
+def define_lattice_tiling(n_rows: int, n_columns: int) -> np.ndarray:
+    row_order_a = [ii % 2 for ii in range(n_columns)]
+    row_order_b = [ii % 2 for ii in range(1, n_columns + 1)]
+    tiling = []
+    for ii in range(n_rows):
+        if ii % 2 == 0:
+            tiling += row_order_a
+        else:
+            tiling += row_order_b
+
+    return np.array(tiling)
