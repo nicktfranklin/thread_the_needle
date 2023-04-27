1fdc74424e081d68d0a70df0e7ea07760d6b74e1
Nick Franklin <nthompsonfranklin@gmail.com>
2023-04-27 14:32:20 -0400

    minor

--- state_inference/model.py
+++ state_inference/model.py
@@ -7,8 +7,6 @@ from torch import nn, optim
 from torch.distributions.categorical import Categorical
 from torch.utils.data import DataLoader
 
-from state_inference.utils import gumbel_softmax
-
 if torch.backends.mps.is_available():
     DEVICE = torch.device("mps")
 elif torch.cuda.is_available():
@@ -143,6 +141,70 @@ class Decoder(MLP):
     ):
         super().__init__(input_size, hidden_sizes, output_size, dropout)
         self.net.pop(-1)
+
+
+# Note: there is an issue F.gumbel_softmax that appears to causes an error w
+# where a valide distribution will return nans, preventing training.  Fix from
+# https://gist.github.com/GongXinyuu/3536da55639bd9bfdd5a905ebf3ab88e
+def gumbel_softmax(
+    logits: torch.Tensor,
+    tau: float = 1,
+    hard: bool = False,
+    dim: int = -1,
+) -> torch.Tensor:
+    r"""
+    Samples from the `Gumbel-Softmax distribution`_ and optionally discretizes.
+    You can use this function to replace "F.gumbel_softmax".
+    Args:
+      logits: `[..., num_features]` unnormalized log probabilities
+      tau: non-negative scalar temperature
+      hard: if ``True``, the returned samples will be discretized as one-hot vectors,
+            but will be differentiated as if it is the soft sample in autograd
+      dim (int): A dimension along which softmax will be computed. Default: -1.
+    Returns:
+      Sampled tensor of same shape as `logits` from the Gumbel-Softmax distribution.
+      If ``hard=True``, the returned samples will be one-hot, otherwise they will
+      be probability distributions that sum to 1 across `dim`.
+    .. note::
+      This function is here for legacy reasons, may be removed from nn.Functional in the future.
+    .. note::
+      The main trick for `hard` is to do  `y_hard - y_soft.detach() + y_soft`
+      It achieves two things:
+      - makes the output value exactly one-hot
+      (since we add then subtract y_soft value)
+      - makes the gradient equal to y_soft gradient
+      (since we strip all other gradients)
+    Examples::
+        >>> logits = torch.randn(20, 32)
+        >>> # Sample soft categorical using reparametrization trick:
+        >>> F.gumbel_softmax(logits, tau=1, hard=False)
+        >>> # Sample hard categorical using "Straight-through" trick:
+        >>> F.gumbel_softmax(logits, tau=1, hard=True)
+    .. _Gumbel-Softmax distribution:
+        https://arxiv.org/abs/1611.00712
+        https://arxiv.org/abs/1611.01144
+    """
+
+    def _gen_gumbels():
+        gumbels = -torch.empty_like(logits).exponential_().log()
+        if torch.isnan(gumbels).sum() or torch.isinf(gumbels).sum():
+            # to avoid zero in exp output
+            gumbels = _gen_gumbels()
+        return gumbels
+
+    gumbels = _gen_gumbels()  # ~Gumbel(0,1)
+    gumbels = (logits + gumbels) / tau  # ~Gumbel(logits,tau)
+    y_soft = gumbels.softmax(dim)
+
+    if hard:
+        # Straight through.
+        index = y_soft.max(dim, keepdim=True)[1]
+        y_hard = torch.zeros_like(logits).scatter_(dim, index, 1.0)
+        ret = y_hard - y_soft.detach() + y_soft
+    else:
+        # Reparametrization trick.
+        ret = y_soft
+    return ret
 
 
 class mDVAE(nn.Module):
@@ -285,7 +347,9 @@ class PomdpModel(nn.Module):
             tau=tau,
             gamma=gamma,
         )
-        self.transition_model = transition_model(transition_model_hidden, z_dim, z_layers)
+        self.transition_model = transition_model(
+            transition_model_hidden, z_dim, z_layers
+        )
 
     def train_state_model(
         self,
