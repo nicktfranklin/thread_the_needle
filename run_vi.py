647240603a7f809f87bfe55ba8b56b3db4968c26
Nicholas Franklin <nthompsonfranklin@gmail.com>
2023-08-24 15:22:31 -0400

    refactoring, update batch simluation script

--- run_vi.py
+++ run_vi.py
@@ -1,90 +1,171 @@
+import os
+from datetime import date
+
 import numpy as np
 import pandas as pd
+import torch
 import yaml
+from tqdm import trange
 
-from state_inference.gridworld_env import ThreadTheNeedleEnv
+from state_inference.gridworld_env import CnnWrapper, ThreadTheNeedleEnv
 from state_inference.model.agents import ViAgentWithExploration
-from state_inference.model.vae import DEVICE, MlpDecoder, MlpEncoder, StateVae
-from state_inference.utils.config_utils import parse_task_config
-from state_inference.utils.training_utils import train_model
+from state_inference.model.vae import MlpDecoder, MlpEncoder, StateVae
+from state_inference.utils.config_utils import (
+    load_config,
+    parse_model_config,
+    parse_task_config,
+)
+from state_inference.utils.pytorch_utils import (
+    DEVICE,
+    convert_8bit_to_float,
+    make_tensor,
+)
+from state_inference.utils.training_utils import get_policy_prob, vae_get_pmf
 
-CONFIG_FILE = "state_inference/env_config.yml"
+######## File Names and Configs ########
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+CONFIG_PATH = "state_inference/configs"
+TASK_CONFIG_FILE = "env_config.yml"
+VAE_CONFIG_FILE = "vae_config.yml"
+AGENT_CONFIG_FILE = "agent_config.yml"
+
 TASK_NAME = "thread_the_needle"
+MODEL_NAME = "cnn_vae"
+
 TASK_CLASS = ThreadTheNeedleEnv
-OUTPUT_FILE_NAME = "ThreadTheNeedleSims.csv"
+AgentClass = ViAgentWithExploration
+
+SIMULATIONS_PATH = "simulations"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 
-##### Model Kwargs
-def make_vi(task):
-    N_EPOCHS = 20  # should be 20
-    EMBEDDING_LAYERS = 5
+def get_value_function(model, task):
+    obs = convert_8bit_to_float(
+        torch.stack(
+            [
+                make_tensor(task.observation_model(s))
+                for s in range(task.transition_model.n_states)
+                for _ in range(1)
+            ]
+        )
+    )[:, None, ...].to(DEVICE)
+    z = model.state_inference_model.get_state(obs)
 
-    LR = 3e-4
-    beta = 1.0
-    tau = 2.0
-    gamma = 0.99
-    dropout = 0.0
-    N_ACTIONS = 4
-
-    EMBEDDING_DIM = len(task.observation_model.states) // 2
-    OBSERVATION_DIM = task.observation_model.map_height**2
-
-    # create the model
-    encoder_hidden = [OBSERVATION_DIM // 5, OBSERVATION_DIM // 10]
-    decoder_hidden = [OBSERVATION_DIM // 10, OBSERVATION_DIM // 5]
-    z_dim = EMBEDDING_DIM * EMBEDDING_LAYERS
-
-    encoder = MlpEncoder(
-        OBSERVATION_DIM,
-        encoder_hidden,
-        z_dim,
-        dropout=dropout,
+    hash_vector = np.array(
+        [
+            model.state_inference_model.z_dim**ii
+            for ii in range(model.state_inference_model.z_layers)
+        ]
     )
 
-    decoder = MlpDecoder(
-        z_dim,
-        decoder_hidden,
-        OBSERVATION_DIM,
-        dropout=dropout,
+    z = z.dot(hash_vector)
+
+    value_function = np.array(
+        [model.value_function.get(z0, np.nan) for z0 in z]
+    ).reshape(20, 20)
+    return value_function
+
+
+def simulate_agent(task, agent_config, vae_config, env_kwargs, get_pmf_fn: callable):
+    agent = AgentClass.make_from_configs(task, agent_config, vae_config, env_kwargs)
+    agent.learn(total_timesteps=agent_config["n_train_steps"], progress_bar=False)
+
+    pmf = get_policy_prob(
+        agent,
+        get_pmf_fn,
+        n_states=env_kwargs["n_states"],
+        map_height=env_kwargs["map_height"],
+        cnn=True,
     )
 
-    vae_kwargs = dict(
-        z_dim=EMBEDDING_DIM, z_layers=EMBEDDING_LAYERS, beta=beta, tau=tau, gamma=gamma
-    )
+    v = get_value_function(agent, task)
 
-    vae_model = StateVae(encoder, decoder, **vae_kwargs).to(DEVICE)
-
-    return ViAgentWithExploration(task, vae_model, set_action=set(range(N_ACTIONS)))
+    return pmf, v
 
 
-def train():
-    env_kwargs, training_kwargs = parse_task_config(TASK_NAME, CONFIG_FILE)
+def train_batches():
+    ## Load Configs
+    task_config_file = os.path.join(CONFIG_PATH, TASK_CONFIG_FILE)
+    vae_config_file = os.path.join(CONFIG_PATH, VAE_CONFIG_FILE)
+    agent_config_file = os.path.join(CONFIG_PATH, AGENT_CONFIG_FILE)
 
-    # create the task
-    task = TASK_CLASS.create_env(**env_kwargs)
+    env_kwargs = parse_task_config(TASK_NAME, task_config_file)
+    vae_config = parse_model_config(MODEL_NAME, vae_config_file)
+    agent_config = load_config(agent_config_file)
 
+    # create the task and get the optimal policy
+    task = CnnWrapper(TASK_CLASS.create_env(**env_kwargs))
     pi, _ = task.get_optimal_policy()
-    training_kwargs["optimal_policy"] = pi
 
-    results = []
+    # label each room with a mask for scoring function
+    room_1_mask = (np.arange(400) < 200) * (np.arange(400) % 20 < 10)
+    room_2_mask = (np.arange(400) >= 200) * (np.arange(400) % 20 < 10)
+    room_3_mask = np.arange(400) % 20 >= 10
 
-    def append_res(results, rewards, model_name):
-        results.append(
-            {
-                "Rewards": rewards,
-                "Model": [model_name] * (training_kwargs["n_epochs"] + 1),
-                "Epoch": [ii for ii in range(training_kwargs["n_epochs"] + 1)],
-            }
+    scores = []
+    value_functions = []
+
+    for idx in trange(agent_config["n_batches"]):
+        pmf, v = simulate_agent(task, agent_config, vae_config, env_kwargs, vae_get_pmf)
+
+        score_room_1 = np.sum(pi[room_1_mask] * pmf[room_1_mask], axis=1).mean()
+        score_room_2 = np.sum(pi[room_2_mask] * pmf[room_2_mask], axis=1).mean()
+        score_room_3 = np.sum(pi[room_3_mask] * pmf[room_3_mask], axis=1).mean()
+
+        scores.append(
+            pd.DataFrame(
+                {
+                    "Iteration": [idx] * 4,
+                    "Score": [
+                        np.sum(pi * pmf, axis=1).mean(),
+                        score_room_1,
+                        score_room_2,
+                        score_room_3,
+                    ],
+                    "Condition": ["Overall", "Room 1", "Room 2", "Room 3"],
+                }
+            )
         )
 
-    agent = make_vi(task)
-    vi_rewards = train_model(agent, **training_kwargs)
+        value_functions.append(
+            pd.DataFrame(
+                {
+                    "Iteration": [idx] * task.n_states,
+                    "State-Values": v.reshape(-1),
+                    "States": np.arange(task.n_states),
+                }
+            )
+        )
 
-    append_res(results, vi_rewards, "Agent")
+    scores = pd.concat(scores)
+    value_functions = pd.concat(value_functions)
 
-    results = pd.concat([pd.DataFrame(res) for res in results])
-    results.set_index("Epoch").to_csv(OUTPUT_FILE_NAME)
+    # save scores
+    scores.to_csv(
+        os.path.join(
+            SIMULATIONS_PATH, f"scores_{TASK_NAME}_{MODEL_NAME}_{date.today()}.csv"
+        )
+    )
+
+    # save value function
+    value_functions.to_csv(
+        os.path.join(
+            SIMULATIONS_PATH, f"value_func_{TASK_NAME}_{MODEL_NAME}_{date.today()}.csv"
+        )
+    )
+
+    # save parameters
+    simulations_parameters = {
+        "Agent": agent_config,
+        "Task": env_kwargs,
+        "VAE": vae_config,
+    }
+    param_file = os.path.join(
+        SIMULATIONS_PATH, f"params_{TASK_NAME}_{MODEL_NAME}_{date.today()}.yml"
+    )
+    with open(param_file, "w") as f:
+        yaml.dump(simulations_parameters, f)
 
 
 if __name__ == "__main__":
-    train()
+    train_batches()
--- state_inference/utils/config_utils.py
+++ state_inference/utils/config_utils.py
@@ -12,18 +12,6 @@ def parse_model_config(model_name, config_file):
     return config[model_name]
 
 
-def parse_task_config(task, config_file):
+def parse_task_config(task_name, config_file):
     config = load_config(config_file)
-
-    env_kwargs = config[task]["env_kwargs"]
-
-    training_kwargs = config["training_kwargs"]
-    training_kwargs.update(
-        dict(
-            n_states=env_kwargs["n_states"],
-            map_height=env_kwargs["map_height"],
-            test_start_state=config[task]["test_start_state"],
-        )
-    )
-
-    return env_kwargs, training_kwargs
+    return config[task_name]["env_kwargs"]
