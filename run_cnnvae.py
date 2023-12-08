8058c45bc2ec5171f31ebb1085cf9b098ee85c3b
Nicholas Franklin <nthompsonfranklin@gmail.com>
2023-12-07 19:53:18 -0500

    added parse function

--- run_cnnvae.py
+++ run_cnnvae.py
@@ -1,4 +1,4 @@
-# import argparse
+import argparse
 import os
 import sys
 from datetime import date
@@ -12,7 +12,7 @@ from stable_baselines3.common.monitor import Monitor, load_results
 from model.vae import MlpDecoder, MlpEncoder, StateVae
 from model.value_iteration import ValueIterationAgent
 from task.gridworld import CnnWrapper, ThreadTheNeedleEnv
-from utils.config_utils import load_config, parse_model_config, parse_task_config
+from utils.config_utils import parse_configs
 from utils.pytorch_utils import DEVICE
 
 print(f"python {sys.version}")
@@ -20,18 +20,14 @@ print(f"torch {torch.__version__}")
 print(f"device = {DEVICE}")
 
 
-# ### Configuration files
-# parser = argparse.ArgumentParser()
+### Configuration files
+parser = argparse.ArgumentParser()
 
-# parser.add_argument("--task_config", default="configs/env_config.yaml")
-
-CONFIG_PATH = "configs"
-TASK_CONFIG_FILE = "env_config.yml"
-VAE_CONFIG_FILE = "vae_config.yml"
-AGENT_CONFIG_FILE = "agent_config.yml"
-
-TASK_NAME = "thread_the_needle"
-MODEL_NAME = "cnn_vae"
+parser.add_argument("--vae_config", default="configs/vae_config.yaml")
+parser.add_argument("--task_config", default="config/env_config.yaml")
+parser.add_argument("--agent_config", default="config/agent_config.yaml")
+parser.add_argument("--task_name", default="thread_the_needle")
+parser.add_argument("--model_name", default="cnn_vae")
 
 # Create log dir
 LOG_DIR = "tmp/"
@@ -41,23 +37,17 @@ TASK_CLASS = ThreadTheNeedleEnv
 AgentClass = ValueIterationAgent
 
 ## Load Configs
-task_config_file = os.path.join(CONFIG_PATH, TASK_CONFIG_FILE)
-vae_config_file = os.path.join(CONFIG_PATH, VAE_CONFIG_FILE)
-agent_config_file = os.path.join(CONFIG_PATH, AGENT_CONFIG_FILE)
-
-env_kwargs = parse_task_config(TASK_NAME, task_config_file)
-vae_config = parse_model_config(MODEL_NAME, vae_config_file)
-agent_config = load_config(agent_config_file)
+config = parse_configs(parser.parse_args())
 
 # create the task and get the optimal policy
-task = CnnWrapper(TASK_CLASS.create_env(**env_kwargs))
+task = CnnWrapper(TASK_CLASS.create_env(**config["env_kwargs"]))
 pi, _ = task.get_optimal_policy()
 
 SAVE_FILE_NAME = f"simulations/thread_the_needle_viagent_{date.today()}.csv"
 
 
 # create the task and get the optimal policy
-task = TASK_CLASS.create_env(**env_kwargs)
+task = TASK_CLASS.create_env(**config["env_kwargs"])
 task = CnnWrapper(task)
 
 # create the monitor
@@ -68,12 +58,11 @@ pi, _ = task.get_optimal_policy()
 
 ### Model + Training Parameters
 
-vae_config = parse_model_config(MODEL_NAME, vae_config_file)
-agent_config = load_config(agent_config_file)
-
 
 def make_model():
-    agent = AgentClass.make_from_configs(task, agent_config, vae_config, env_kwargs)
+    agent = AgentClass.make_from_configs(
+        task, config["agent_config"], config["vae_config"], config["env_kwargs"]
+    )
     return agent
 
 
