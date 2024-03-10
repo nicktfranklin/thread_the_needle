abfb13f2e94dd256663e0d6c52da2b9c238c7566
Nicholas Franklin <nthompsonfranklin@gmail.com>
2024-03-10 13:48:13 -0400

    change import

--- model/agents/__init__.py
+++ model/agents/__init__.py
@@ -1,4 +1,5 @@
 from .oracle import Oracle
 from .ppo import PPO
+from .recurrent_vi_agent import RecurrentViAgent
 from .utils.base_agent import BaseAgent
 from .value_iteration import ValueIterationAgent
