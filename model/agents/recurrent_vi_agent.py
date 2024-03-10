862eae3c951211ef2cfddc1a6565a722a625ccde
Nicholas Franklin <nthompsonfranklin@gmail.com>
2024-03-10 13:55:57 -0400

    start of new recurrent implementation

--- model/agents/recurrent_vi_agent.py
+++ model/agents/recurrent_vi_agent.py
@@ -1,15 +1,14 @@
 from random import choice
-from typing import Any, Dict, List, Optional, Set, Tuple
+from typing import Any, Dict, Hashable, List, Optional, Set, Tuple
 
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch import Tensor
+from torch import FloatTensor, Tensor
 from torch.utils.data import DataLoader
 
 from model.agents.constants import (
     ALPHA,
-    BATCH_LENGTH,
     BATCH_SIZE,
     EPSILON,
     GAMMA,
@@ -20,8 +19,9 @@ from model.agents.constants import (
     SOFTMAX_GAIN,
 )
 from model.agents.value_iteration import ValueIterationAgent
-from model.data.d4rl import OaroTuple
+from model.data.d4rl import D4rlDataset, OaroTuple
 from model.state_inference.recurrent_vae import LstmVae
+from task.utils import ActType
 from utils.data import RecurrentVaeDataset, TransitionVaeDataset
 from utils.pytorch_utils import DEVICE, convert_8bit_to_float, maybe_convert_to_tensor
 
@@ -31,7 +31,6 @@ class RecurrentViAgent(ValueIterationAgent):
         self,
         task,
         state_inference_model: LstmVae,
-        set_action: Set[int],
         optim_kwargs: Dict[str, Any] | None = None,
         grad_clip: bool = GRAD_CLIP,
         batch_size: int = BATCH_SIZE,
@@ -39,157 +38,203 @@ class RecurrentViAgent(ValueIterationAgent):
         n_iter: int = N_ITER_VALUE_ITERATION,
         softmax_gain: float = SOFTMAX_GAIN,
         epsilon: float = EPSILON,
-        batch_length: int = BATCH_LENGTH,
+        n_steps: Optional[int] = None,  # None => only update at the end,
         n_epochs: int = N_EPOCHS,
         alpha: float = ALPHA,
+        dyna_updates: int = 5,
         max_sequence_len: int = MAX_SEQUENCE_LEN,
     ) -> None:
         super().__init__(
-            task,
-            state_inference_model,
-            set_action,
-            optim_kwargs,
-            grad_clip,
-            batch_size,
-            gamma,
-            n_iter,
-            softmax_gain,
-            epsilon,
-            batch_length,
-            n_epochs,
-            alpha,
+            task=task,
+            state_inference_model=state_inference_model,
+            optim_kwargs=optim_kwargs,
+            grad_clip=grad_clip,
+            batch_size=batch_size,
+            gamma=gamma,
+            n_iter=n_iter,
+            softmax_gain=softmax_gain,
+            epsilon=epsilon,
+            n_steps=n_steps,
+            n_epochs=n_epochs,
+            alpha=alpha,
+            dyna_updates=dyna_updates,
         )
         self.max_sequence_len = max_sequence_len
 
-    @staticmethod
-    def construct_dataloader_from_obs(
-        batch_size: int, list_obs: List[Tensor], max_seq_len: int = MAX_SEQUENCE_LEN
-    ) -> DataLoader:
-        obs = torch.stack([obs.obs for obs in list_obs])
-        actions = F.one_hot(torch.tensor([obs.a for obs in list_obs]))
-        index = [obs.index for obs in list_obs]
-        return RecurrentVaeDataset.construct_dataloader(
-            obs, actions, index, max_seq_len, batch_size
-        )
+    def _init_state(self):
+        return None
 
-    def _prep_vae_dataloader(
-        self,
-        batch_size: int,
-    ):
-        r"""
-        preps the dataloader for training the State Inference VAE
-
-        Args:
-            batch_size (int): The number of samples per batch
-        """
-        return RecurrentViAgent.construct_dataloader_from_obs(
-            batch_size, self.rollout_buffer.get_all(), self.max_sequence_len
-        )
-
-    def contruct_validation_dataloader(self, sample_size, seq_len):
-        # assert (
-        #     sample_size % seq_len == 0
-        # ), "Sample size must be an interger multiple of sequence length"
-        validation_obs = []
-        for t in range(sample_size // seq_len):
-            obs_prev = self.task.reset()[0]
-
-            for _ in range(seq_len):
-                action = choice(list(self.action_space))
-                obs, rew, terminated, _, _ = self.task.step(action)
-                obs_tuple = OaroTuple(
-                    obs=torch.tensor(obs_prev),
-                    a=action,
-                    r=rew,
-                    next_obs=torch.tensor(obs),
-                    index=t,
-                )
-                validation_obs.append(obs_tuple)
-
-                if terminated:
-                    break
-        return RecurrentViAgent.construct_dataloader_from_obs(
-            batch_size=len(validation_obs), list_obs=validation_obs, max_seq_len=seq_len
-        )
-
-    def _precalculate_states_for_batch_training(self) -> Tuple[Tensor, Tensor]:
-        raise NotImplementedError
-
-    def _init_state(self) -> Tensor:
-        # unbatch state for the agent (not vae training)
-        return torch.zeros(
-            self.state_inference_model.z_dim * self.state_inference_model.z_layers
-        )
-
-    def _get_state_hashkey(self, obs: Tensor, state_prev: Optional[Tensor]):
-        obs = maybe_convert_to_tensor(obs)
+    def _preprocess_obs(self, obs: Tensor) -> Tensor:
+        raise NotImplementedError()
+        # take in 8bit with shape NxHxWxC
+        # convert to float with shape NxCxHxW
         obs = convert_8bit_to_float(obs)
-        with torch.no_grad():
-            state = self.state_inference_model.get_state(obs, state_prev)
-        return state.dot(self.hash_vector)
+        if obs.ndim == 3:
+            return obs.permute(2, 0, 1)
+        return obs.permute(0, 3, 1, 2)
 
-    def _update_hidden_state(self, obs: Tensor, state: Tensor) -> Tensor:
-        return self.state_inference_model.update_hidden_state(obs, state)
+    def _get_state_hashkey(self, obs: Tensor):
+        raise NotImplementedError()
+        obs = obs if isinstance(obs, Tensor) else torch.tensor(obs)
+        obs_ = self._preprocess_obs(obs)
+        with torch.no_grad():
+            z = self.state_inference_model.get_state(obs_)
+        return z.dot(self.hash_vector)
+
+    def update_rollout_policy(self, rollout_buffer: D4rlDataset) -> None:
+        raise NotImplementedError()
+        # the rollout policy is a DYNA variant
+        # dyna updates (note: this assumes a deterministic enviornment,
+        # and this code differes from dyna as we are only using resampled
+        # values and not seperately sampling rewards and sucessor states
+
+        # pass the obseration tuple through the state-inference network
+        obs, a, r, next_obs = rollout_buffer.get_obs(-1)
+        s = self._get_state_hashkey(obs)[0]
+        sp = self._get_state_hashkey(next_obs)[0]
+
+        # update the model
+        self.transition_estimator.update(s, a, sp)
+        self.reward_estimator.update(sp, r)
+
+        # update q-values
+        self.update_qvalues(s, a, r, sp)
+
+        # resampling (dyna) updates
+        for _ in range(min(len(rollout_buffer), self.n_dyna_updates)):
+            # sample observations and actions with replacement
+            idx = random.randint(0, len(rollout_buffer) - 1)
+
+            obs, a, _, _ = rollout_buffer.get_obs(idx)
+
+            s = self._get_state_hashkey(obs)[0]
+
+            # draw r, sp from the model
+            sp = self.transition_estimator.sample(s, a)
+            r = self.reward_estimator.sample(sp)
+
+            self.update_qvalues(s, a, r, sp)
+
+    def get_policy(self, obs: Tensor):
+        raise NotImplementedError()
+        s = self._get_state_hashkey(obs)
+        p = self.policy.get_distribution(s)
+        return p
+
+    def get_pmf(self, obs: FloatTensor) -> np.ndarray:
+        raise NotImplementedError()
+        return self.get_policy(obs).distribution.probs.clone().detach().numpy()
 
     def predict(
-        self,
-        obs: Tensor,
-        state: Tensor,
-        episode_start=None,
-        deterministic: bool = False,
-    ) -> tuple[Tensor, Tensor]:
-        obs = maybe_convert_to_tensor(obs)
-        obs = convert_8bit_to_float(obs)
-
-        # e-greedy sampling
+        self, obs: Tensor, state=None, episode_start=None, deterministic: bool = False
+    ) -> tuple[ActType, None]:
+        raise NotImplementedError()
         if not deterministic and np.random.rand() < self.policy.epsilon:
-            a = torch.tensor([np.random.randint(self.policy.n_actions)])
-        else:
-            hashed_sucessor_state = self._get_state_hashkey(obs, state)
+            return np.random.randint(self.policy.n_actions), None
 
-            p = self.policy.get_distribution(hashed_sucessor_state)
+        s = self._get_state_hashkey(obs)
+        p = self.policy.get_distribution(s)
+        return p.get_actions(deterministic=deterministic).item(), None
 
-            # sample the action
-            a = p.get_actions(deterministic=deterministic)
+    def update_model(self, obs: OaroTuple):
+        raise NotImplementedError()
+        s, a, r, sp = self._get_sars_tuples(obs)
+        self.transition_estimator.update(s, a, sp)
+        self.reward_estimator.update(sp, r)
 
-        # update the RNN Hidden state
-        next_state = self._update_hidden_state(obs, state)
+    def update_qvalues(self, s, a, r, sp) -> None:
+        raise NotImplementedError()
+        """
+        This is the Dyna-Q update rule. From Sutton and Barto (2020), ch 8
+        """
 
-        return a, next_state
+        self.policy.maybe_init_q_values(s)
+        self.policy.maybe_init_q_values(sp)
 
-    def _update_rollout_policy(
-        self, obs: OaroTuple, state: None, state_prev: None
-    ) -> None:
-        # prep the observations for the RNN
-        o = convert_8bit_to_float(obs.obs[None, ...]).to(DEVICE)
-        op = convert_8bit_to_float(obs.next_obs[None, ...]).to(DEVICE)
+        q_s_a = self.policy.q_values[s][a]
+        V_sp = max(self.policy.q_values[sp].values())
 
-        state = state.view(1, -1).to(DEVICE)
-        state_prev = state_prev.view(1, -1).to(DEVICE)
+        q_s_a += self.alpha * (r + self.gamma * V_sp - q_s_a)
+        self.policy.q_values[s][a] = q_s_a
 
-        # pass through the RNN to get the embedding
-        _, z = self.state_inference_model.encode_from_state(o, state)
-        _, zp = self.state_inference_model.encode_from_state(op, state_prev)
+    def get_state_values(self) -> torch.Tensor:
+        raise NotImplementedError()
+        return self.policy.get_value_function()
 
-        # convert from one hot tensor to int array
-        s = torch.argmax(z, dim=-1).detach().cpu().view(-1).numpy()
-        sp = torch.argmax(zp, dim=-1).detach().cpu().view(-1).numpy()
+    def train_vae(self, buffer: D4rlDataset, progress_bar: bool = True):
+        raise NotImplementedError()
+        # prepare the dataset for training the VAE
+        dataset = buffer.get_dataset()
+        obs = convert_8bit_to_float(torch.tensor(dataset["observations"])).to(DEVICE)
+        obs = obs.permute(0, 3, 1, 2)  # -> NxCxHxW
 
-        s = s.dot(self.hash_vector)
-        sp = sp.dot(self.hash_vector)
-        self.update_qvalues(s, obs.a, obs.r, sp)
-
-
-class ViControlableStateInf(ValueIterationAgent):
-    def _prep_vae_dataloader(self, batch_size: int = BATCH_SIZE):
-        obs = self.rollout_buffer.get_tensor("obs")
-        obsp = self.rollout_buffer.get_tensor("obsp")
-        a = F.one_hot(self.rollout_buffer.get_tensor("a"), num_classes=4).float()
-
-        dataset = TransitionVaeDataset(obs, a, obsp)
-
-        return DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=True,
+        dataloader = DataLoader(
+            obs, batch_size=self.batch_size, shuffle=True, drop_last=True
         )
+
+        # train the VAE on the rollouts
+        self.state_inference_model.train_epochs(
+            self.n_epochs,
+            dataloader,
+            self.optim,
+            self.grad_clip,
+            progress_bar=progress_bar,
+        )
+
+    def update_from_batch(self, buffer: D4rlDataset, progress_bar: bool = False):
+        raise NotImplementedError()
+        self.train_vae(buffer, progress_bar=progress_bar)
+
+        # re-estimate the reward and transition functions
+        self.reward_estimator.reset()
+        self.transition_estimator.reset()
+
+        dataset = buffer.get_dataset()
+
+        # convert ot a tensor dataset for iteration
+        dataset = ViDataset(dataset)
+
+        # _get_hashed_state takes care of preprocessing
+        dataloader = DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=False, drop_last=False
+        )
+        s, sp, a, r = [], [], [], []
+        for batch in dataloader:
+            s.append(self._get_state_hashkey(batch["observations"]))
+            sp.append(self._get_state_hashkey(batch["next_observations"]))
+            a.append(batch["actions"])
+            r.append(batch["rewards"])
+        s = np.concatenate(s)
+        sp = np.concatenate(sp)
+        a = np.concatenate(a)
+        r = np.concatenate(r)
+
+        for idx in range(len(s)):
+            self.transition_estimator.update(s[idx], a[idx], sp[idx])
+            self.reward_estimator.update(sp[idx], r[idx])
+
+        # use value iteration to estimate the rewards
+        self.policy.q_values, value_function = value_iteration(
+            t=self.transition_estimator.get_transition_functions(),
+            r=self.reward_estimator,
+            gamma=self.gamma,
+            iterations=self.n_iter,
+        )
+        self.value_function = value_function
+
+    @classmethod
+    def make_from_configs(
+        cls,
+        task,
+        agent_config: Dict[str, Any],
+        vae_config: Dict[str, Any],
+        env_kwargs: Dict[str, Any],
+    ):
+        raise NotImplementedError()
+        VaeClass = getattr(model.state_inference.vae, agent_config["vae_model_class"])
+        vae = VaeClass.make_from_configs(vae_config, env_kwargs)
+        return cls(task, vae, **agent_config["state_inference_model"])
+
+    def get_graph_laplacian(self) -> tuple[np.ndarray, Dict[Hashable, int]]:
+        raise NotImplementedError()
+        return self.transition_estimator.get_graph_laplacian()
