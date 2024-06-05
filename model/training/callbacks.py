15d271d0d651224e21e194306439645a87513907
Nicholas Franklin <nthompsonfranklin@gmail.com>
2024-06-04 20:56:19 -0400

    add callback for atari performance

--- model/training/callbacks.py
+++ model/training/callbacks.py
@@ -3,6 +3,58 @@ from stable_baselines3.common.callbacks import BaseCallback
 from model.training.scoring import score_model
 
 
+class AtariCallback(BaseCallback):
+    def __init__(self, verbose: int = 0):
+        super().__init__(verbose)
+        self.rewards = dict(num_timesteps=[], rewards=[])
+        self.evaluations = []
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        pass
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+        self.evaluations.append({"num_timesteps": self.num_timesteps, "reward": 0})
+
+    def _on_step(self) -> bool:
+        """
+        This method will be called by the model after each call to `env.step()`.
+
+        For child callback (of an `EventCallback`), this will be called
+        when the event is triggered.
+
+        :return: If the callback returns False, training is aborted early.
+        """
+        self.rewards["num_timesteps"].append(self.num_timesteps)
+        if "rewards" in self.locals:
+            self.rewards["rewards"].append(self.locals["rewards"].item())
+        elif "reward" in self.locals:
+            self.rewards["rewards"].append(self.locals["reward"])
+        else:
+            print(self.locals)
+            raise ValueError("No reward found in locals")
+        return True
+
+    def _on_rollout_end(self) -> None:
+        """
+        This event is triggered before updating the policy.
+        """
+        pass
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
+
+
 class ThreadTheNeedleCallback(BaseCallback):
     """
     A custom callback that derives from ``BaseCallback``.
