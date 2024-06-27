98241dabf6fff4972d5eb6900679f150e0310049
Nicholas Franklin <nthompsonfranklin@gmail.com>
2024-06-27 19:03:06 -0400

    continued work on dppo

--- model/agents/discrete_ppo.py
+++ model/agents/discrete_ppo.py
@@ -1,17 +1,21 @@
-from typing import Any, Dict, Optional
+import inspect
+from typing import Any, Dict, Hashable, Optional
 
 import gymnasium as gym
 import numpy as np
+import torch
 from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.policies import ActorCriticPolicy
+
+# from stable_baselines3.common.policies import ActorCriticPolicy
 from torch import FloatTensor, Tensor
 
 import model.state_inference.vae
 from model.agents.utils.base_agent import BaseAgent
+from model.state_inference.nets.mlp import MLP
 from model.state_inference.vae import StateVae
 from model.training.rollout_data import RolloutBuffer
 from task.utils import ActType
-from utils.pytorch_utils import DEVICE
+from utils.pytorch_utils import DEVICE, convert_8bit_to_float
 
 
 class DiscretePPO(BaseAgent):
@@ -20,7 +24,6 @@ class DiscretePPO(BaseAgent):
         self,
         env: gym.Env,
         state_inference_model: StateVae,
-        policy: ActorCriticPolicy,
         optim_kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
         """
@@ -37,9 +40,46 @@ class DiscretePPO(BaseAgent):
             ]
         )
 
-        self.policy = policy
+        input_size = (
+            self.state_inference_model.z_dim * self.state_inference_model.z_layers
+        )
+
+        self.policy = MLP(
+            input_size=input_size,
+            hidden_sizes=[input_size],
+            output_size=env.action_space.n,
+        )
+
+        self.critic = MLP(
+            input_size=input_size,
+            hidden_sizes=[input_size],
+            output_size=1,
+        )
+
+    def _init_state(self):
+        return None
+
+    def _preprocess_obs(self, obs: Tensor) -> Tensor:
+        # take in 8bit with shape NxHxWxC
+        # convert to float with shape NxCxHxW
+        obs = convert_8bit_to_float(obs)
+        if obs.ndim == 3:
+            return obs.permute(2, 0, 1)
+        return obs.permute(0, 3, 1, 2)
+
+    def _get_state_hashkey(self, obs: Tensor):
+        obs = obs if isinstance(obs, Tensor) else torch.tensor(obs)
+        obs_ = self._preprocess_obs(obs)
+        with torch.no_grad():
+            z = self.state_inference_model.get_state(obs_)
+        return z.dot(self.hash_vector)
 
     def get_policy(self, obs: Tensor):
+        """
+        assume obs is shape (NxHxWxC).
+
+        Not used for training~
+        """
         raise NotImplementedError()
 
     def get_pmf(self, obs: FloatTensor) -> np.ndarray:
@@ -123,7 +163,36 @@ class DiscretePPO(BaseAgent):
         vae_config: Dict[str, Any],
         env_kwargs: Dict[str, Any],
     ):
-        raise NotImplementedError
         VaeClass = getattr(model.state_inference.vae, agent_config["vae_model_class"])
         vae = VaeClass.make_from_configs(vae_config, env_kwargs)
-        return cls(task, vae, **agent_config["state_inference_model"])
+
+        # figure out what the accepatble args and kwargs are
+        # Get the signature of the __init__ method of DiscretePPO
+        sig = inspect.signature(cls.__init__)
+
+        # Iterate through the parameters to separate args and kwargs
+        args = []
+        kwargs = []
+        for param in sig.parameters.values():
+            # Skip 'self' parameter for methods
+            if param.name == "self":
+                continue
+            if param.default == inspect.Parameter.empty:
+                args.append(param.name)
+            else:
+                kwargs.append(param.name)
+
+        print(args, kwargs)
+
+        state_inference_kwargs = {
+            k: v
+            for k, v in agent_config["state_inference_model"].items()
+            if k in args + kwargs
+        }
+        print(state_inference_kwargs)
+        return cls(task, vae, **state_inference_kwargs)
+
+    def get_graph_laplacian(
+        self, normalized: bool = True
+    ) -> tuple[np.ndarray, Dict[Hashable, int]]:
+        return self.transition_estimator.get_graph_laplacian(normalized=normalized)
