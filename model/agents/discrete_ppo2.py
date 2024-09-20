4724a5b436f70e303a8af0e691139f48aef0980c
Nicholas Franklin <nthompsonfranklin@gmail.com>
2024-09-20 15:56:42 -0400

    wrapper code arround SB PPO for new DPPO implementation

--- model/agents/discrete_ppo2.py
+++ model/agents/discrete_ppo2.py
@@ -1,5 +1,4 @@
-import warnings
-from typing import ClassVar, Dict, Hashable, List, Tuple, Type, Union
+from typing import ClassVar, Dict, List, Type
 
 import gymnasium as gym
 import numpy as np
@@ -7,25 +6,20 @@ import torch
 import torch.nn.functional as F
 from gymnasium import spaces
 from stable_baselines3 import PPO as WrappedPPO
+from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.type_aliases import PyTorchObs
-from stable_baselines3.common.utils import explained_variance, get_schedule_fn
-
-# from stable_baselines3.common.policies import ActorCriticPolicy
-from torch import FloatTensor, Tensor, nn
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
+from stable_baselines3.common.utils import (
+    explained_variance,
+    get_schedule_fn,
+    obs_as_tensor,
+)
+from stable_baselines3.common.vec_env import VecEnv
+from torch import FloatTensor
 
 import model.state_inference.vae
-from model.agents.stable_baseline_clone.policies import (
-    ActorCriticCnnPolicy,
-    ActorCriticPolicy,
-    BasePolicy,
-)
+from model.agents.stable_baseline_clone.policies import ActorCriticCnnPolicy, BasePolicy
 from model.agents.utils.base_agent import BaseAgent
-from model.agents.utils.data import PpoDataset
-from model.state_inference.nets.mlp import MLP
-from model.state_inference.vae import StateVae
-from model.training.rollout_data import PpoBuffer
+from model.training.buffers import RolloutBuffer
 from task.utils import ActType
 from utils.pytorch_utils import DEVICE, convert_8bit_to_float
 
@@ -38,9 +32,7 @@ class DiscretePpo2(WrappedPPO, BaseAgent):
     policy_aliases: ClassVar[Dict[str, Type[BasePolicy]]] = {
         "CnnPolicy": ActorCriticCnnPolicy,
     }
-
-    def update_from_batch(self, buffer: PpoBuffer, progress_bar: bool = False):
-        raise NotImplementedError
+    rollout_buffer: RolloutBuffer
 
     def get_pmf(self, obs: FloatTensor) -> FloatTensor:
         return (
@@ -53,14 +45,167 @@ class DiscretePpo2(WrappedPPO, BaseAgent):
     def get_value_fn(self, x: FloatTensor) -> FloatTensor:
         raise NotImplementedError
 
-    def get_state_hashkey(self, obs: Tensor) -> Hashable:
-        raise NotImplementedError
-
     def _setup_model(self) -> None:
-        super()._setup_model()
-        print(self.policy)
+        self._setup_lr_schedule()
+        self.set_random_seed(self.seed)
 
-        # raise NotImplementedError
+        if self.rollout_buffer_class is None:
+            if isinstance(self.observation_space, spaces.Dict):
+                raise NotImplementedError("Dict observation space is not supported")
+            else:
+                self.rollout_buffer_class = RolloutBuffer
+
+        self.rollout_buffer = self.rollout_buffer_class(
+            self.n_steps,
+            self.observation_space,  # type: ignore[arg-type]
+            self.action_space,
+            device=self.device,
+            gamma=self.gamma,
+            gae_lambda=self.gae_lambda,
+            n_envs=self.n_envs,
+            **self.rollout_buffer_kwargs,
+        )
+        print(type(self.rollout_buffer))
+        self.policy = self.policy_class(  # type: ignore[assignment]
+            self.observation_space,
+            self.action_space,
+            self.lr_schedule,
+            use_sde=self.use_sde,
+            **self.policy_kwargs,
+        )
+        self.policy = self.policy.to(self.device)
+
+        # Initialize schedules for policy/value clipping
+        self.clip_range = get_schedule_fn(self.clip_range)
+        if self.clip_range_vf is not None:
+            if isinstance(self.clip_range_vf, (float, int)):
+                assert self.clip_range_vf > 0, (
+                    "`clip_range_vf` must be positive, "
+                    "pass `None` to deactivate vf clipping"
+                )
+
+            self.clip_range_vf = get_schedule_fn(self.clip_range_vf)
+
+    def collect_rollouts(
+        self,
+        env: VecEnv,
+        callback: BaseCallback,
+        rollout_buffer: RolloutBuffer,
+        n_rollout_steps: int,
+    ) -> bool:
+        """
+        Collect experiences using the current policy and fill a ``RolloutBuffer``.
+        The term rollout here refers to the model-free notion and should not
+        be used with the concept of rollout used in model-based RL or planning.
+
+        :param env: The training environment
+        :param callback: Callback that will be called at each step
+            (and at the beginning and end of the rollout)
+        :param rollout_buffer: Buffer to fill with rollouts
+        :param n_rollout_steps: Number of experiences to collect per environment
+        :return: True if function returned with at least `n_rollout_steps`
+            collected, False if callback terminated rollout prematurely.
+        """
+        assert self._last_obs is not None, "No previous observation was provided"
+        # Switch to eval mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(False)
+
+        n_steps = 0
+        rollout_buffer.reset()
+        print(type(rollout_buffer))
+        # Sample new weights for the state dependent exploration
+        if self.use_sde:
+            self.policy.reset_noise(env.num_envs)
+
+        callback.on_rollout_start()
+
+        while n_steps < n_rollout_steps:
+            if (
+                self.use_sde
+                and self.sde_sample_freq > 0
+                and n_steps % self.sde_sample_freq == 0
+            ):
+                # Sample a new noise matrix
+                self.policy.reset_noise(env.num_envs)
+
+            with torch.no_grad():
+                # Convert to pytorch tensor or to TensorDict
+                obs_tensor = obs_as_tensor(self._last_obs, self.device)
+                actions, values, log_probs = self.policy(obs_tensor)
+            actions = actions.cpu().numpy()
+
+            # Rescale and perform action
+            clipped_actions = actions
+
+            if isinstance(self.action_space, spaces.Box):
+                if self.policy.squash_output:
+                    # Unscale the actions to match env bounds
+                    # if they were previously squashed (scaled in [-1, 1])
+                    clipped_actions = self.policy.unscale_action(clipped_actions)
+                else:
+                    # Otherwise, clip the actions to avoid out of bound error
+                    # as we are sampling from an unbounded Gaussian distribution
+                    clipped_actions = np.clip(
+                        actions, self.action_space.low, self.action_space.high
+                    )
+
+            new_obs, rewards, dones, infos = env.step(clipped_actions)
+
+            self.num_timesteps += env.num_envs
+
+            # Give access to local variables
+            callback.update_locals(locals())
+            if not callback.on_step():
+                return False
+
+            self._update_info_buffer(infos)
+            n_steps += 1
+
+            if isinstance(self.action_space, spaces.Discrete):
+                # Reshape in case of discrete action
+                actions = actions.reshape(-1, 1)
+
+            # Handle timeout by bootstraping with value function
+            # see GitHub issue #633
+            for idx, done in enumerate(dones):
+                if (
+                    done
+                    and infos[idx].get("terminal_observation") is not None
+                    and infos[idx].get("TimeLimit.truncated", False)
+                ):
+                    terminal_obs = self.policy.obs_to_tensor(
+                        infos[idx]["terminal_observation"]
+                    )[0]
+                    with torch.no_grad():
+                        terminal_value = self.policy.predict_values(terminal_obs)[0]  # type: ignore[arg-type]
+                    rewards[idx] += self.gamma * terminal_value
+
+            # ~~~~~ Modified ~~~~~
+            # save and log the successor observations
+            rollout_buffer.add(
+                self._last_obs,  # type: ignore[arg-type]
+                new_obs,
+                actions,
+                rewards,
+                self._last_episode_starts,  # type: ignore[arg-type]
+                values,
+                log_probs,
+            )
+            # ~~~~~~~~~~~~~~~~~~~~
+            self._last_obs = new_obs  # type: ignore[assignment]
+            self._last_episode_starts = dones
+
+        with torch.no_grad():
+            # Compute value for the last timestep
+            values = self.policy.predict_values(obs_as_tensor(new_obs, self.device))  # type: ignore[arg-type]
+
+        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
+
+        callback.update_locals(locals())
+
+        callback.on_rollout_end()
+
+        return True
 
     def train(self) -> None:
         """
