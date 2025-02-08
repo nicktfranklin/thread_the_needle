3f7d867afeb62542fd0f67a11917af0210c616ee
Nicholas Franklin <nthompsonfranklin@gmail.com>
2025-02-08 09:54:38 -0500

    bug fix

--- src/thread_the_needle/__init__.py
+++ src/thread_the_needle/__init__.py
@@ -3,8 +3,8 @@ from .gridworld import OpenEnv, ThreadTheNeedleEnv
 
 def make(task_name, *args, **kwargs):
     if task_name == "thread_the_needle":
-        return ThreadTheNeedleEnv(*args, **kwargs)
+        return ThreadTheNeedleEnv.make(*args, **kwargs)
     elif task_name == "open":
-        return OpenEnv(*args, **kwargs)
+        return OpenEnv.make(*args, **kwargs)
     else:
         raise ValueError("Unknown task name: {}".format(task_name))
